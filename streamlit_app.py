6ec77a9b4529021ff4965656cd5a35cd0645ed85
jananisnowflake <135587720+jananisnowflake@users.noreply.github.com>
2023-06-05 13:35:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale,Spinach &  Rocket Smoothie')
+streamlit.text('Hard-boiled free-range egg')
+
